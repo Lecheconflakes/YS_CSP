e2772362dcded5a8a17f07f1c254c0419ce14c0d
Lecheconflakes <yaretzi.sanchez@UCAS-edu.net>
2025-09-02 13:38:40 -0600

    programed variables practice

--- practices/variable_practice.py
+++ practices/variable_practice.py
@@ -1,2 +1,14 @@
 #YS 6th variable practice
 
+name=input("what is your name?")
+number_Main=input("pick a number betweem 10 and 1?")
+Number_Minor=input("pick another number between 100 amd 100?")
+Choice_Breakfast=input("what did you eat this morning?")
+color=input("what is your favortie color?")
+School=input("what school do you attend?")
+Year=input("what year is it currently?")
+Eye_color=input("what is your eye color?")
+Age=input("how old are you?")
+Subject= input("what is your favorite subject in school?: ")
+
+
