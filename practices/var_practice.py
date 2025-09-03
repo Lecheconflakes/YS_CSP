fac65256748885e6c173fb1489c20a1df4189fcc
Lecheconflakes <yaretzi.sanchez@ucas-edu.net>
2025-09-03 05:48:04 +0000

    var_practice

--- practices/var_practice.py
+++ practices/var_practice.py
@@ -2,7 +2,7 @@
 
 name=input("what is your name?\n")
 number_Main=input("pick a number betweem 10 and 1?\n")
-Number_Minor=input("pick another number between 100 and 1000\n?")
+Number_Minor=input("pick another number between 100 and 1000?\n")
 Choice_Breakfast=input("what did you eat this morning?\n")
 color=input("what is your favortie color?\n")
 School=input("what school do you attend?\n")
@@ -12,4 +12,4 @@ Age=input("how old are you?\n")
 Subject= input("what is your favorite subject in school?\n")
 
 
-print("what is your name?: ", name,"pick a number between 10 and 1?: ", number_Main, "pick another number between 100 and 1000?:",Number_Minor , "what did you eat this morning?:", Choice_Breakfast, "Whats your favortie color?: ", color, "what school do you attend?: ", School, "What year is it curently?: ", Year, "whats your eye color?: ", Eye_color, "How old are you?: ", Age, "what is your favorite subject at school?: ", Subject)
+print("what is your name?: ", name,"\n", "pick a number between 10 and 1?: ", number_Main,"\n", "pick another number between 100 and 1000?:",Number_Minor ,"\n", "what did you eat this morning?:", Choice_Breakfast,"\n", "Whats your favortie color?: ", color,"\n", "what school do you attend?: ", School,"\n", "What year is it curently?: ", Year,"\n", "whats your eye color?: ", Eye_color,"\n", "How old are you?: ", Age,"\n", "what is your favorite subject at school?: ", Subject,"\n")
