ab7cda7d5e14167ec9dee74205a27334518181e9
Lecheconflakes <yaretzi.sanchez@UCAS-edu.net>
2025-09-04 13:44:39 -0600

    exprestions

---
+++ Notes/expressions.py
@@ -0,0 +1,55 @@
+# YS 6th exprestion notes
+
+#what is a algorithm?
+    # An algorithm is a set of insurction to accoplish a task.
+    #Sarah had 10 apples to give to 6 friends. How many apples does each person get? 10/6 = 1
+    # An expresstion is a 
+
+name=input("what is your name? ")
+print("hello", name)
+
+ # algorithm for area
+length=5
+width=20
+area= length * width 
+print(length, "*", width, "=", area )
+
+
+#List steps in an algorithm
+    # step 1:  what variables/pieces of information are needed.
+    # step 2: procces information.
+    # step 3:  Provide an output.
+
+
+#average age of a group of 4
+age_one= 14
+age_two=14
+age_three=15
+age_four=15
+
+total= age_one + age_two + age_three + age_four 
+average=total /4 
+print( "average age", age_one, "-", age_two, "-", age_three, "-", age_four, "/", 4, "=", average )
+
+#List ALL of the different mathematical operators (Give me the symbol and tell me what it does)
+     # math equations
+num_one=12
+num_two=4
+print("addtion", num_one + num_two)
+print("subrtaction", num_one - num_two)
+print("multiply", num_one * num_two)
+print("division", num_one / num_two)
+print("exponets", num_one ** num_two)
+print("integer divistion", num_one // num_two)
+print("modulo (remander)", num_one % num_two)
+
+
+#List ALL of the different assignment operators (Give me the symbols and what it does)
+    # + is adding    # - is subtract   # * is multiply # / is divide
+
+
+#Why are expressions so important in programming?
+    #
+
+
+ 
