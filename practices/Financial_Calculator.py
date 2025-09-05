e831b477db5d1b7dc58b00ba97e8650a40675289
Lecheconflakes <yaretzi.sanchez@UCAS-edu.net>
2025-09-05 13:44:24 -0600

    finanical calculator

---
+++ practices/Financial_Calculator.py
@@ -0,0 +1,12 @@
+#income $3000 input
+#rent 1200 input
+#untilties 250 input
+# groceries 200 input
+#transportation 500 input
+          #income 
+#savings (3000*.1) 
+               #rent/income*100
+#percent_rent; 1200/300 48%
+
+#total expenses: 2150 (rent + utitliies + groceries+ transportation)
+#spending: 550 (income- total expenses - savings)
