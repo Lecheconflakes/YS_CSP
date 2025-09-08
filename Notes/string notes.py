15b4a01cfe9f6ba04554a0a15310a020800f4e1f
Lecheconflakes <yaretzi.sanchez@UCAS-edu.net>
2025-09-08 13:44:41 -0600

    upated notes

--- Notes/string notes.py
+++ Notes/string notes.py
@@ -1,3 +1,54 @@
 # YS 6th string notes
 
 print("i did it!")
+
+#What makes something a string? 
+# just a collection of symbols held together by quotation marks. 
+"content of a string"
+'this is also a string'
+
+#example: 
+name = input('what is your name?\n').strip().title()
+
+#name = input('what is your name?\n').strip().capitalize()
+
+#name = input('what is your name?\n').strip().upper()
+
+#name = input('what is your name?\n').strip().lower()
+
+#name = input('what is your name?\n').strip()
+
+first_name = input("what is your first name?\n")
+
+last_name + input
+
+" "
+ 
+sentence= " the quick brown fox jumps over the lazy dog."
+
+print("welcome to my program", name)
+
+#why do we have strings?
+  #strings are the only data that allow us to use letters
+
+
+#How do stupid proofing and sanitization relate to each other?
+
+
+#What is debugging?
+
+#How do you debug the different types of errors?
+
+#Describe what each of the methods below does:
+
+#find()
+
+#concatenate (add)
+
+#upper() #name = input('what is your name?\n').strip().upper()
+
+#lower()name = input('what is your name?\n').strip().lower()
+
+#strip() name = input('what is your name?\n').strip()
+
+#name = input('what is your name?\n').strip().capitalize()
