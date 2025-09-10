a31d63de3c98945f8f5b631eff6311db9e23c82b
Lecheconflakes <yaretzi.sanchez@UCAS-edu.net>
2025-09-10 13:46:44 -0600

    progess of silly sentcnece

---
+++ practices/silly_sentences.py
@@ -0,0 +1,10 @@
+#YS 6th silly sentences
+
+noun_name1 = input('give me a name\n').strip().capitalize()
+noun_name2 = input(' give me another name\n').strip().capitalize()
+noun_thing1 = input('give me a noun\n').strip().capitalize()
+noun_name2 = input('give me another noun\n').strip().capitalize()
+noun_name3 = input('give me one more noun, I promise its the last noun\n').strip().capitalize()
+adjective = input("give me a adjective\n").strip().capitalize()
+print(noun_name1, "and", noun_name2 )
+ #shshshs
