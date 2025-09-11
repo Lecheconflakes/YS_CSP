36dd48c9f091ac43d9fe1866d85ce3bbe4c8767e
Lecheconflakes <yaretzi.sanchez@ucas-edu.net>
2025-09-11 01:19:53 +0000

    finshed silly sentence

--- practices/silly_sentences.py
+++ practices/silly_sentences.py
@@ -3,8 +3,8 @@
 noun_name1 = input('give me a name\n').strip().capitalize()
 noun_name2 = input(' give me another name\n').strip().capitalize()
 noun_thing1 = input('give me a noun\n').strip().capitalize()
-noun_name2 = input('give me another noun\n').strip().capitalize()
-noun_name3 = input('give me one more noun, I promise its the last noun\n').strip().capitalize()
+noun_thing2 = input('give me another noun\n').strip().capitalize()
+noun_thing3 = input('give me one more noun, I promise its the last noun\n').strip().capitalize()
 adjective = input("give me a adjective\n").strip().capitalize()
-print(noun_name1, "and", noun_name2 )
- #shshshs
+print(noun_name1, "and", noun_name2, "are going to make a sandwich. First they spread on", noun_thing1,". Then, they add some", noun_thing2, "and", noun_thing3, " their sandwiches are", adjective, "!"  )
+ 
