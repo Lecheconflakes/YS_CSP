679ab4075025a3baa5af347c56d12b3b1c0698ce
Lecheconflakes <yaretzi.sanchez@UCAS-edu.net>
2025-09-18 13:44:09 -0600

    old enough

---
+++ practices/old_enough.py
@@ -0,0 +1,16 @@
+#YS 6th Old enough
+
+age = int(input("tell me your age?\n"))
+
+if age >= 18:
+    print(" you are old enough to vote!")
+elif age <= 17 and age >= 16:17
+    print( " you are old enough to to drive!")
+elif age == 15:
+    print(" you are old enogh to get a learns permit!")
+elif age <= 4:
+    print(" you are old enough to go to school!")
+else:
+    age >= 3
+    print("go play outside you ipad kid")
+    
