3d2081b61f1a6f688fc9e6c4e2a76bae5f7f6dd4
Lecheconflakes <yaretzi.sanchez@ucas-edu.net>
2025-09-20 03:02:48 +0000

    finshed time of day

--- practices/time_of_day.py
+++ practices/time_of_day.py
@@ -1,10 +1,15 @@
 #YS 6th time of day
 
-import datetime
+import time
 
-current = datetime.datetime.now()
-hour = current.hour 
-  
+hour = time.localtime().tm_hour
 
-if
+if hour >= 0 and hour < 6:
+    print("Good  Early morning!")
+elif hour >= 6 and hour < 12:
+    print("Good morning!")
+elif hour >= 12 and hour < 18:
+    print("Good afternoon!")
+else:
+    print("Good evening!")
 
