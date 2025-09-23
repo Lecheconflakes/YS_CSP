46246eeaf51655a37829cc0e0705cca77b0974fd
Lecheconflakes <yaretzi.sanchez@UCAS-edu.net>
2025-09-23 13:39:17 -0600

    fizz buzz finshed

---
+++ practices/fizz_buzz.py
@@ -0,0 +1,14 @@
+#YS 6th FizzBuzz
+for x  in range (1,51):
+     if x%3 ==0 and x%5 ==0:
+         print("fizzbuzz")
+     elif x % 3 ==0:
+        print(f"fizz")
+     elif x % 5 ==0:
+         print(f"buzz")
+     else:
+         print(x)
+
+
+ 
+ 
