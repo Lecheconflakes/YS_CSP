1d6cd01b257a0868e0e73399285cca9a5ac7935d
Lecheconflakes <yaretzi.sanchez@ucas-edu.net>
2025-10-06 13:44:03 -0600

    notes and practice

---
+++ c_programming/practices/variables_practice.c
@@ -0,0 +1,17 @@
+// YS 6th variables practice
+#include <stdio.h>
+
+int main(void){
+    char name[25];
+    float GPA;
+    
+
+
+
+
+    printf("tell me a your name: ");
+    fgets(name, sizeof(name), stdin);
+
+return 0;
+
+}
