65ce982853b00afaa63e9a9c9cd747cdc0e74b3b
Lecheconflakes <yaretzi.sanchez@ucas-edu.net>
2025-10-07 17:11:40 +0000

    finshed expresstions practice

---
+++ c_programming/practices/expresstion_practice.c
@@ -0,0 +1,44 @@
+// YS 6th expresstion practice
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    //equation 1
+    int eq1 = 7-24/8* 4+6;
+    printf("equation 1 = %d\n", eq1);
+
+    //equation 2
+    int eq2 = 18/3 -7 +2 *5;
+    printf("equation 2 = %d\n", eq2);
+
+    //equation 3
+    int eq3 = 6*4/12+72/8-9;
+    printf("equation 3 = %d\n", eq3);
+
+    //equation 4
+    int eq4 = (17-6/2)+4*3;
+    printf("equation 4 = %d\n", eq4);
+
+    //equation 5 
+    int eq5 = -2*(1*4-2/2)+ (6+2-3);
+    printf("equation 5 = %d\n", eq5);
+
+    //equation 6
+    int eq6 = -1*((3-4*7)/5) -2 *24/6;
+    printf("equation 6 = %d\n", eq6);
+
+    // eqaution 7
+    int eq7 = (3* pow(5, 2)/15)- (5- pow(2,2));
+    printf(" equation 6 = %d\n", eq7);
+
+    // equation 8
+    int eq8 = (pow(1, 4)* pow(2, 2)+ pow(3,3))- pow(2,5)/ 4;
+    printf("equation 8 = %d\n", eq8);
+
+    //equation 9
+    int eq9 = pow((22/2-2*5), 2) + pow((4-6/6),2);
+    printf("equation 9 = %d\n", eq9);
+
+    return 0;
+}
