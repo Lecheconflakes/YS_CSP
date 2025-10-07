ad2dd8c9978229bb419317c5a4693380f4428fbe
Lecheconflakes <yaretzi.sanchez@ucas-edu.net>
2025-10-07 04:05:06 +0000

    practice

--- c_programming/practices/variables_practice.c
+++ c_programming/practices/variables_practice.c
@@ -1,17 +1,29 @@
 // YS 6th variables practice
 #include <stdio.h>
 
-int main(void){
-    char name[25];
-    float GPA;
+int main(){
+    char name[] = "Yaretzi Sanchez";
+    int lucky_number = 4;
+    float gpa = 3.95;
+    char breakfast[] = "pan dulce on cafe";
+    char school_name = "UCAS";
+    int current_year = 2025;
+    char eye_color = "brown";
+    int age = 15;
+    char favorite_subject = "Biology";
+
+    printf("%s\n",name);
+    printf("%d\n", lucky_number);
+    printf("%.2f\n", gpa);
+    printf("%s\n", breakfast);
+    printf("%s\n", favorite_subject);
+    printf("%s\n", school_name);
+    printf("%d\n",current_year);
+    printf("%s\n", eye_color);
+    printf("%d\n", age);
+    printf("%s\n",favorite_subject);
     
-
-
-
-
-    printf("tell me a your name: ");
-    fgets(name, sizeof(name), stdin);
-
-return 0;
+    return 0;
 
 }
+
