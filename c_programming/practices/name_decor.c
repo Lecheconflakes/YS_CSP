d67cbadda5e7bbf4ec4bbc7641041d79da9cad4d
Lecheconflakes <yaretzi.sanchez@ucas-edu.net>
2025-10-11 04:08:48 +0000

    finshed name decor

--- c_programming/practices/name_decor.c
+++ c_programming/practices/name_decor.c
@@ -5,14 +5,16 @@
 int main(void){
     char name[100];
     char deco_name[100];
-
+    char decoration[] = " ⋆˙⊹❀♡";
+ 
     printf("Please enter your name: \n");
     scanf("%s", name);
 
 
-    strcat( deco_name,"⋆˙⊹❀♡⁭");
+    strcat( deco_name, decoration);
     strcat(deco_name, name);
-    strcat(deco_name, "♡❀˖⁺. ༶");
+    strcat(deco_name, decoration);
+   
     printf("%s",deco_name);
 
     return 0;
