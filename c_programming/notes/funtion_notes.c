c394b1488b1bc5a417074c525b6f904d01315f43
Lecheconflakes <yaretzi.sanchez@ucas-edu.net>
2025-10-14 13:46:04 -0600

    functions notes

---
+++ c_programming/notes/funtion_notes.c
@@ -0,0 +1,67 @@
+// YS 6th function notes
+
+#include<stdio.h>
+
+void brithday(char* name, int age){
+    printf("happy birthday to you\n");
+    printf("happy birthday to you\n");
+    printf("happy birthday dear %s\n", "tia");
+    printf("happy birthday to you\n");
+    printf("happy brithday %s is now %d\n", "tia", age);
+
+}
+
+int add(int num_1, int num_2){
+    return num_1 = num_2;
+}
+
+char* get_name(void){
+    char *name;
+    print("what is your name: ");
+    scanf("%s", name);
+    return name;
+}
+
+int main(void){
+  brithday("tia", 15);
+  brithday("tia", 15);
+  brithday("theo", 20);
+int addtion = add(5,4);
+char* user = get_name();
+prinf("%d\n", add(5,7));
+printf("%d\n", add(50,37));
+
+
+ return 0;
+}
+
+
+
+//What a function is
+    // like a variable but it stores infomation and ,
+
+//Why we use functions
+    //so we can have multpabe easier way to code and for the main function
+
+//How to write a function in C
+    // we do not write functon inside another function
+
+//What are arguments and parameters?
+    //the terms "arguments" and "parameters" relate to how data is passed to functions.
+
+//How do arguments and parameters work together?
+    //
+
+//How to use parameters and arguments in C
+    // for parameters we put what we want to chnage in the output
+
+//What are return statements?
+    // taking one thing abd replacing it the call with it
+
+//How do return statements change how you define a function in C?
+
+
+//What do return statements do with the information?
+
+
+
