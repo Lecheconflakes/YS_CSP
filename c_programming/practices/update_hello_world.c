d4fc4b941fb767454a5d9bfa1c007a85d07f40cd
Lecheconflakes <yaretzi.sanchez@ucas-edu.net>
2025-10-16 05:06:24 +0000

    fisished hello world

--- c_programming/practices/update_hello_world.c
+++ c_programming/practices/update_hello_world.c
@@ -2,6 +2,16 @@
 
 #include <stdio.h> 
 
+void greet(const char* name) {
+    printf("hello %s\n", name);
+}
+
 int main(){
-    
+    greet("Dom");
+    greet("Mia");
+    greet("Brian");
+    greet("Letty");
+    greet("Suki");
+
+    return 0;
 }
